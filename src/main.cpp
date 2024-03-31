987804036355ec354a6263014aaee5a20d4ff934
SAITO, Tetsuya <saito.tetsuya@gmail.com>
2024-03-31 10:30:50 +0900

    main.hppへ切り出し

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,4 @@
-#if defined(ARDUINO_M5STACK_DIAL)
-#include <M5Dial.h>
-inline void M5_BEGIN(bool enableEncoder = false, bool enableRFID = false) {
-    M5Dial.begin(enableEncoder, enableRFID);
-}
-inline void M5_BEGIN(m5::M5Unified::config_t cfg, bool enableEncoder = false,
-                     bool enableRFID = false) {
-    M5Dial.begin(cfg, enableEncoder, enableRFID);
-}
-inline void M5_UPDATE(void) {
-    M5.update();
-}
-inline M5GFX& M5_DISPLAY(void) {
-    return M5Dial.Display;
-}
-#else
-#include <M5Unified.h>
-inline void M5_BEGIN(void) {
-    M5.begin();
-}
-inline void M5_UPDATE(void) {
-    M5.update();
-}
-inline M5GFX& M5_DISPLAY(void) {
-    return M5.Display;
-}
-#endif
+#include "main.hpp"
 
 #include "lvgl_port_m5stack.hpp"
 
